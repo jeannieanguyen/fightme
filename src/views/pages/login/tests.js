33ab822b574809f0ea91ed284c2a5173aba982f9
Lee Richardson <lee@getvictorious.com>
2017-10-05 17:10:35 -0700

    Add mocha, chai, enzyme and related dependencies. Test suite runs.

---
+++ src/views/pages/login/tests.js
@@ -0,0 +1,13 @@
+// import { expect } from 'chai';
+// import React, { component } from 'react';
+// import { shallow, mount } from 'enzyme';
+// import LoginPage from './login';
+// 
+// describe('Login Page Component', () => {
+// 
+//   it ('renders an email input', () => {
+//     const el = shallow(<LoginPage />);
+// 
+//     expect(el.find('input[name=email]')).to.have.length(1);
+//   });
+// });
