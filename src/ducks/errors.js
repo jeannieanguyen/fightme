2e5f314668555621bccd139096b51d67ab98f6c8
wendygwo <wgwo@hotmail.com>
2017-10-12 12:14:06 -0700

    Fix linter errors for ducsk/errors.js

--- src/ducks/errors.js
+++ src/ducks/errors.js
@@ -1,29 +1,27 @@
 export const CATCHALL_ERROR = 'CATCHALL_ERROR';
 
-export const setGeneralError = (errorMessage) => {
-    return {
-        type: CATCHALL_ERROR,
-        error: errorMessage,
-    }
-}
+export const setGeneralError = errorMessage => ({
+  type: CATCHALL_ERROR,
+  error: errorMessage,
+});
 
 const INITIAL_STATE = {
-    error: null
+  error: null,
 };
 
-export default function (state=INITIAL_STATE, action) {
-    switch(action.type) {
-        case CATCHALL_ERROR:
-            return {
-                ...state,
-                error: action.error,
-            }
-        default:
-          return {
-            ...state,
-            error: null,
-          }
-    }
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case CATCHALL_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
+    default:
+      return {
+        ...state,
+        error: null,
+      };
+  }
 }
 
-export const getGeneralErrorSelector = (state) => state.errors.error;
+export const getGeneralErrorSelector = state => state.errors.error;
