d5bf6ddd99dc6cd1e5a8d713151acb9d55d7a4b6
wendygwo <wgwo@hotmail.com>
2017-10-19 10:50:25 -0700

    Updates per PR comments

--- src/ducks/auth/selectors.js
+++ src/ducks/auth/selectors.js
@@ -1,13 +1,17 @@
+import { get } from 'lodash';
+
 export const getUser = state => state.auth.user;
 export const getSample = state => state.auth.sample;
 
-export const getRegister = (state) => {
-  let registeredUser;
+export const getRegisteredUserDetails = (state) => {
+  let registeredUserDetails;
   if (state.auth.register) {
-    const { user, userConfirmed, userSub } = state.auth.register;
-    const username = user.username;
-    registeredUser = { username, userConfirmed, userSub };
+    const username = get(state.auth.register, 'user.username', '');
+    const userConfirmed = get(state.auth.register, 'userConfirmed', false);
+    const userSub = get(state.auth.register, 'userSub', '');
+
+    registeredUserDetails = { username, userConfirmed, userSub };
   }
 
-  return registeredUser;
+  return registeredUserDetails;
 };
--- src/views/pages/login/index.js
+++ src/views/pages/login/index.js
@@ -36,7 +36,7 @@ class RegisterPage extends Component {
       <div className="form-container">
         <div>
           <h1>REGISTER USER</h1>
-          { this.props.register && <h2> You&apos;re registered! Go to Login. </h2> }
+          { this.props.registeredUserDetails && <h2> You&apos;re registered! Go to Login. </h2> }
           <label htmlFor="email_field">E-mail</label>
           <input
             type="text"
@@ -65,7 +65,7 @@ class RegisterPage extends Component {
 
 RegisterPage.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  register: PropTypes.shape({
+  registeredUserDetails: PropTypes.shape({
     username: PropTypes.string,
     userConfirmed: PropTypes.bool,
     userSub: PropTypes.string,
@@ -73,12 +73,12 @@ RegisterPage.propTypes = {
 };
 
 RegisterPage.defaultProps = {
-  register: null,
+  registeredUserDetails: null,
 };
 
 function mapStateToProps(state) {
   return {
-    register: authSelectors.getRegister(state),
+    registeredUserDetails: authSelectors.getRegisteredUserDetails(state),
   };
 }
 
