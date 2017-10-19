1d8a82f3a26e8794a8a0bdccf7fcb72075c4ac6c
Lee Richardson <lee@getvictorious.com>
2017-10-18 18:40:54 -0700

    Remove runScripts property from testSetup.js. Add globals to jsdom so RxJS can use them in test env

--- testSetup.js
+++ testSetup.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 import { JSDOM } from 'jsdom';
 
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>', { runScripts: 'dangerously' });
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const { window } = jsdom;
 
 function copyProps(src, target) {
@@ -23,4 +23,14 @@ global.apigClientFactory = {
   newClient: () => {},
 };
 global.React = React;
+// Add these global objects into JSDOM so RxJS can use them
+window.Array = Array;
+window.Object = Object;
+window.String = String;
+window.Function = Function;
+window.RegExp = RegExp;
+window.Math = Math;
+window.Number = Number;
+window.Date = Date;
+window.parseInt = parseInt;
 copyProps(window, global);
