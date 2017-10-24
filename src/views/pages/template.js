5a861be25219009f0278b41c87c0772d32df6aa2
Jeannie Nguyen <jeannie.jujubee@gmail.com>
2017-10-24 12:56:31 -0700

    Fix lint again

--- src/views/pages/template.js
+++ src/views/pages/template.js
@@ -4,30 +4,34 @@ import PropTypes from 'prop-types';
 
 function mapStateToProps(state) {
   return {
-    ...state
+    ...state,
   };
 }
 
 export class TemplateView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      prop_key: '',
+    };
   }
 
   render() {
     return (
       <div id="template-page">
         <h1>TemplateView</h1>
+        { this.props.prop_key }
       </div>
     );
   }
 }
 
 TemplateView.propTypes = {
+  prop_key: PropTypes.string,
 };
 
 TemplateView.defaultProps = {
+  prop_key: '',
 };
 
 export default connect(mapStateToProps, {})(TemplateView);
-
-
